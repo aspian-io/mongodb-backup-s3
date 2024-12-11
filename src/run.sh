cc6757b2fc3bca1f7aff6af9c461d6a446596808
Omid Rouhani <o.rohani@gmail.com>
2024-12-11 23:02:36 +0330

    updated run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-set -e
+set -eu
 
 . /env.sh
 
+# Configure AWS S3 Signature Version if required
+if [ "$S3_S3V4" = "yes" ]; then
+  aws configure set default.s3.signature_version s3v4
+fi
+
+# Run the backup immediately if no schedule is provided
 if [ -z "$SCHEDULE" ]; then
-    echo "[INFO] No SCHEDULE provided. Running backup once."
-    /backup.sh
-    exit 0
+  echo "[INFO] No SCHEDULE provided. Running backup once."
+  /backup.sh
 else
-    echo "[INFO] SCHEDULE set to '$SCHEDULE'. Using go-cron for scheduling."
-    echo "[INFO] Starting go-cron with schedule: $SCHEDULE"
-    exec go-cron -s "$SCHEDULE" -p 8080 -- /backup.sh
+  echo "[INFO] SCHEDULE set to '$SCHEDULE'. Using go-cron for scheduling."
+  exec go-cron "$SCHEDULE" /bin/sh /backup.sh
 fi

cb8d9ab59917b565a6c15458c3e8c4e268e44e13
Omid Rouhani <o.rohani@gmail.com>
2024-12-11 23:15:31 +0330

    updated run.sh

--- src/run.sh
+++ src/run.sh
@@ -3,11 +3,6 @@ set -eu
 
 . /env.sh
 
-# Configure AWS S3 Signature Version if required
-if [ "$S3_S3V4" = "yes" ]; then
-  aws configure set default.s3.signature_version s3v4
-fi
-
 # Run the backup immediately if no schedule is provided
 if [ -z "$SCHEDULE" ]; then
   echo "[INFO] No SCHEDULE provided. Running backup once."

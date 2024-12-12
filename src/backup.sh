2e1c7beb0b1b206d6f8b7245ee2cb6542b7fd4fa
Omid Rouhani <o.rohani@gmail.com>
2024-12-12 14:12:42 +0330

    updated backup.sh

--- src/backup.sh
+++ src/backup.sh
@@ -27,8 +27,16 @@ if [ -n "${PASSPHRASE:-}" ]; then
   s3_uri="${s3_uri}.gpg"
 fi
 
+# Initialize AWS CLI arguments
+aws_args=""
+[ -n "${S3_REGION:-}" ] && aws_args+=" --region $S3_REGION"
+[ -n "${S3_ENDPOINT:-}" ] && aws_args+=" --endpoint-url $S3_ENDPOINT"
+
 echo "Uploading backup to $s3_uri..."
-aws $aws_args s3 cp "$backup_file" "$s3_uri"
+aws $aws_args s3 cp "$backup_file" "$s3_uri" || {
+    echo "[ERROR] Failed to upload backup to S3."
+    exit 1
+}
 rm "$backup_file"
 echo "Backup uploaded successfully."
 

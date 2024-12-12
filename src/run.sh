ef62049a027f242295b8f56dbabb34e00d9538e7
Omid Rouhani <o.rohani@gmail.com>
2024-12-12 13:47:13 +0330

    run.sh updated

--- src/run.sh
+++ src/run.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-set -eu
 
 . "$(dirname "$0")/env.sh"
 
-# Run the backup immediately if no schedule is provided
 if [ -z "$SCHEDULE" ]; then
     echo "[INFO] No SCHEDULE provided. Running backup once."
-    /backup.sh
+    /bin/bash /backup.sh
 else
     echo "[INFO] SCHEDULE set to '$SCHEDULE'. Using go-cron for scheduling."
-    exec go-cron "$SCHEDULE" /bin/sh /backup.sh
+    echo "[INFO] Starting go-cron with schedule: $SCHEDULE"
+    exec go-cron "$SCHEDULE" /bin/bash /backup.sh
 fi
